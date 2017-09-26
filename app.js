70cd6f477120bb5ec13686104ec3085c5049bce8
victoriactemple <victoriactemple@gmail.com>
2017-09-26 16:18:22 -0400

    Set up environment variables and MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI); 
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -13,6 +18,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
